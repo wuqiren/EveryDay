d67364e8ce663a98905202b9baba61b241d0f88e
fishfan <fishf3@163.com>
2022-03-03 00:12:47 +0800

    283移动零

--- 算法/算法练习/283移动零.js
+++ 算法/算法练习/283移动零.js
@@ -3,5 +3,19 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
+    let k = 0;
+    let length = nums.length;
+    for (let i = 0; i < length; i++){
+        if(nums[i]!==0){
+            nums[k]= nums[i]
+            k++;
 
+        }
+    }
+    for (let j = k; j < length;j++){
+        nums[j]=0
+    }
+    return nums
 };
+const nums = [0,1,0,3,12]
+console.log(moveZeroes(nums));

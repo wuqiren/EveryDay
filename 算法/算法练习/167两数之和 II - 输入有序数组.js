ef73991c5c10117f735977d3f1a4ce274b3acad6
fishfan <fishf3@163.com>
2022-03-04 00:21:53 +0800

    167 双指针法实现

--- 算法/算法练习/167两数之和 II - 输入有序数组.js
+++ 算法/算法练习/167两数之和 II - 输入有序数组.js
@@ -14,7 +14,21 @@
         map[numbers[i]] = i
     }
 };
-
+var twoSum2 = function(numbers, target) {
+    let left = 0;
+    let right = numbers.length-1
+    while (left <= right){
+        if(numbers[left]+numbers[right]===target){
+            return [left+1,right+1]
+        }
+        if(numbers[left]+numbers[right]>target){
+            right = right-1
+        }
+        if(numbers[left]+numbers[right]<target){
+            left = left+1
+        }
+    }
+};
 const numbers = [2,7,11,15]
 
-console.log(twoSum(numbers,9))
+console.log(twoSum2(numbers,9))

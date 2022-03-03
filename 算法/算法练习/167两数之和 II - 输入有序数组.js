fbd380406d2ce91d499b444483974cfb79168224
fishfan <fishf3@163.com>
2022-03-04 00:10:17 +0800

    167 空间N 时间N的算法提交

--- 算法/算法练习/167两数之和 II - 输入有序数组.js
+++ 算法/算法练习/167两数之和 II - 输入有序数组.js
@@ -4,5 +4,17 @@
  * @return {number[]}
  */
  var twoSum = function(numbers, target) {
-
+    let map ={}
+    for (let i = 0; i < numbers.length; i++){
+        let a = target-numbers[i]
+        if(map[a]!==undefined){
+            console.log(map[a],'map[a]map[a]')
+            return [map[a]+1,i]
+        }
+        map[numbers[i]] = i
+    }
 };
+
+const numbers = [2,7,11,15]
+
+console.log(twoSum(numbers,9))

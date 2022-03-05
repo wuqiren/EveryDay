ff9ea9a04f24cf9e981df23f044467d787b2ef34
fishfan <fishf3@163.com>
2022-03-06 00:17:30 +0800

    344反转字符串简单的方法，使用交换位置的形式

--- 算法/算法练习/344反转字符串.js
+++ 算法/算法练习/344反转字符串.js
@@ -5,3 +5,16 @@
  var reverseString = function(s) {
     return s.reverse()
 };
+
+var reverseString1 = function(s) {
+    for (let i=0;i<=Math.floor((s.length-1)/2);i++) {
+        let y = s[i]
+        s[i]=s[s.length-1-i]
+        s[s.length-1-i] = y
+    }
+    return s
+};
+
+let s = ["h","e","l","l","o"]
+
+console.log(reverseString1(s))

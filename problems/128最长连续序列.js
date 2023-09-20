d4ba98c2461b1116a26770083a5bed81f275d17f
fishfan <fishf3@163.com>
2023-09-20 19:25:57 +0800

    128最长连续序列

---
+++ problems/128最长连续序列.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = function(nums) {
+    let num_set = new Set();
+    for (const num of nums) {
+        num_set.add(num);
+    }
+
+    let longestStreak = 0;
+
+    for (const num of num_set) {
+        if (!num_set.has(num - 1)) {
+            let currentNum = num;
+            let currentStreak = 1;
+
+            while (num_set.has(currentNum + 1)) {
+                currentNum += 1;
+                currentStreak += 1;
+            }
+
+            longestStreak = Math.max(longestStreak, currentStreak);
+        }
+    }
+
+    return longestStreak;   
+};
+
